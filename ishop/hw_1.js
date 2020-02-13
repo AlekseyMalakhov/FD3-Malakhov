5a5ef3642dbd45c69807ac0a799e1674055f272c
TermenVox <hexel@tut.by>
2020-02-13 20:31:49 +0300

    готово

--- ishop/hw_1.js
+++ ishop/hw_1.js
@@ -1,93 +1,77 @@
-/*
-var questionText='Как вы относитесь к программированию?';
-
-var answersArr=[ 
-  {text:'хорошо',code:1,count:222}, 
-  {text:'не получается',code:2,count:123}, 
-  {text:'избегаю',code:3,count:444} 
-];
-
-var VotesBlock = React.createClass({
-
-  displayName: 'VotesBlock',
-
-  propTypes: {
-    question: React.PropTypes.string.isRequired,
-    answers: React.PropTypes.array.isRequired,
-  },
-
-  render: function() {
-
-    var answersCode=this.props.answers.map( v =>
-        React.DOM.div({key:v.code,className:'Answer'},
-          React.DOM.span({className:'Count'},v.count),
-          React.DOM.span({className:'Text'},v.text),
-        )
-      );
-    return React.DOM.div( {className:'VotesBlock'}, 
-      React.DOM.div( {className:'Question'}, this.props.question ),
-      React.DOM.div( {className:'Answers'}, answersCode ),
-    );
-  },
-
-});
-
-ReactDOM.render(
-  React.createElement(VotesBlock,{question:questionText,answers:answersArr}),
-  document.getElementById('container')
-);
-*/
-
-
 var goods = [
   {
     name: "book",
     price: 10,
-    pic_url: "",
+    pic_url: "book.jpg",
     stock: 47
   },
   {
     name: "pencil",
     price: 7,
-    pic_url: "",
+    pic_url: "pencil.jpg",
     stock: 25
   },
   {
     name: "sheet",
     price: 8,
-    pic_url: "",
+    pic_url: "sheet.jpg",
     stock: 32
   },
   {
     name: "glue",
     price: 5,
-    pic_url: "",
+    pic_url: "glue.jpg",
     stock: 27
   },
   {
     name: "ruler",
     price: 6,
-    pic_url: "",
+    pic_url: "ruler.jpg",
     stock: 74
   },
-
 ];
 
-console.log(goods);
+function createRow(goods) {
+  return React.DOM.tr({key: goods.name + "row"}, 
+          React.DOM.td({key: goods.name + 1}, goods.name),
+          React.DOM.td({key: goods.name + 2}, goods.price),
+          React.DOM.td({key: goods.name + 3},
+            React.DOM.img({key: goods.name + 4, src: "pics/" + goods.pic_url}, null), 
+          ),
+          React.DOM.td({key: goods.name + 5}, goods.stock)
+        );
+}
 
 var ShowGoods = React.createClass({
-    displayName: 'ShowGoods',
-  
-    render: function(){  
-      return React.DOM.div( {className:'MyComponentFrame'}, 
-        React.DOM.h1( null, "Hi" ),
-        React.DOM.div( {className:'MyComponentText'}, "all" ),
-      );
-    },
-  
+  displayName: 'ShowGoods',
+
+  propTypes: {
+    goods: React.PropTypes.array.isRequired,
+    shop_name: React.PropTypes.string.isRequired,
+  }, 
+
+  render: function(){ 
+            return React.DOM.div(null,
+                    React.DOM.h3( null, this.props.shop_name),
+                    React.DOM.table( null,
+                      React.DOM.tr({key: "header"}, 
+                        React.DOM.th({key: "name"}, "Name"),
+                        React.DOM.th({key: "price"}, "Price"),
+                        React.DOM.th({key: "pic"}, "Picture"),
+                        React.DOM.th({key: "stock"}, "In stock")
+                      ),
+                      this.props.goods.map(createRow),
+                    ),
+                  );
+  },
 });
 
+var props = {
+  goods: goods,
+  shop_name: "Mega Shop"
+};
+
 ReactDOM.render(
-    React.createElement(ShowGoods), 
+    React.createElement(ShowGoods, props), 
     document.getElementById('root') 
 );
