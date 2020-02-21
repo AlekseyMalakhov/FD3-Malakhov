ffa2c813751177c7abc170dde0a05406965d3e4a
TermenVox <hexel@tut.by>
2020-02-21 21:06:43 +0300

    Update index.js

--- filter/src/index.js
+++ filter/src/index.js
@@ -2,36 +2,90 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+var props = [
+    "создавать",
+    "пользовательские",
+    "создавать интерактивные",
+    "пользовательские интерфейсы",
+    "реакт интерфейсы",
+    "реакт приятно и просто",
+    "приятно",
+    "просто",
+    "части",
+    "части приложения",
+    "выглядят просто",
+    "интерфейса приложения",
+    "приложения",
+    "выглядят",
+    "приложения выглядят",
+];
+
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            match: [],
+            check: false,
+            text: "",
+        };
+        this.createList = this.createList.bind(this);
+        this.checkText = this.checkText.bind(this);
+        this.handleCheckBox = this.handleCheckBox.bind(this);
+        this.cleanInput = this.cleanInput.bind(this);
     }
 
-    render() {
+    createList() {
+        var result;
+        var match = [...this.state.match];
+        if (this.state.check) {
+            result = this.props.txt.map(checkMatch);
+        } else {
+            result = this.props.txt.map((e) => <li key = {e + 25}>{e}</li>);
+        }
+        function checkMatch(e) {
+            if (match.includes(e)) {
+                return <li key = {e + 25}>{e}</li>;
+            }
+        }
+        return result;
+    }
+
+    handleCheckBox(e) {
+        this.setState({check: e.target.checked});
+    }
+
+    checkText(e) {
+        var text = e.target.value;
+        var match = [];
+        this.props.txt.map(checkStr);
+        function checkStr(str) {
+            if (str.indexOf(text) !== -1) {
+                match.push(str);
+            }
+        }
+        this.setState({match: match,
+                        text: text});
+    }
+
+    cleanInput() {
+        this.setState({match: [],
+                        text: ""});
+    }
+
+    render() {  
         return (
             <div className="filt">
                 <div className="panel">
-                    <input type="checkbox"></input>
-                    <input type="text"></input>
-                    <input type="button" value="Сброс"></input>
+                    <input type="checkbox" onChange={this.handleCheckBox}></input>
+                    <input type="text" value = {this.state.text} onChange={this.checkText}></input>
+                    <input type="button" value="Сброс" onClick = {this.cleanInput}></input>
                 </div> 
                  <ul>
-                    <li>Coffee</li>
-                    <li>Tea</li>
-                    <li>Milk</li>
-                    <li>Coffee</li>
-                    <li>Tea</li>
-                    <li>Milk</li>
-                    <li>Coffee</li>
-                    <li>Tea</li>
-                    <li>Milk</li>
-                    <li>Coffee</li>
-                    <li>Tea</li>
-                    <li>Milk</li>
+                   {this.createList()}
                 </ul> 
             </div>
         );
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App txt = {props}/>, document.getElementById('root'));
