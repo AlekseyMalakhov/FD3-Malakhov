c6ef653e7b3d2fc1dcb9b81fea6796f1ca3bffd8
Aleksey Malakhov <hexel@tut.by>
2020-02-27 20:38:07 +0300

    upd

--- ishop3/src/Item.js
+++ ishop3/src/Item.js
@@ -9,7 +9,7 @@ class Item extends React.Component {
     render() {
         var item = this.props.item;
         return (
-            <tr className = {(this.props.selected === item.id) ? "selected" : ""} id = {"row_" + item.id} key = {item.id + "row"} onClick = {()=> this.props.onItemSelect(this.props.item.id)}>
+            <tr className = {(this.props.selected === item.id) ? "selected" : ""} id = {"row_" + item.id} key = {item.id + "row"} onClick = {()=> this.props.onItemSelect(this.props.item)}>
                 <td key = {item.id + item.name}>
                     {item.name}
                 </td>
@@ -23,7 +23,8 @@ class Item extends React.Component {
                     {item.stock}
                 </td>
                 <td key = {item.id + "delete"}>
-                    <input id = {"btn_" + item.id} type="button" value="Delete" key = {item.name + 7} onClick = {()=> this.props.onItemDelete(this.props.item.name, this.props.item.id)}></input>
+                    <input id = {"btn_edit" + item.id} type="button" value="Edit" key = {item.id + 7} onClick = {()=> this.props.onItemEdit(true)}></input>
+                    <input id = {"btn_del" + item.id} type="button" value="Delete" key = {item.id + 8} onClick = {()=> this.props.onItemDelete(this.props.item.name, this.props.item.id)}></input>                    
                 </td>
             </tr> 
 
--- ishop3/src/Shop.js
+++ ishop3/src/Shop.js
@@ -10,13 +10,18 @@ class Shop extends React.Component {
             deleted_item_name: "",
             deleted_item_id: "",
             showRequest: false,
-            selected: "",
+            showCard: false,
+            selected_item: {},
+            edit: false,
         };
         this.createRows = this.createRows.bind(this);
         this.deleteRow = this.deleteRow.bind(this);
         this.hideDeleteRequest = this.hideDeleteRequest.bind(this);
         this.getDeleteData = this.getDeleteData.bind(this);
         this.getSelectData = this.getSelectData.bind(this);
+        this.addRow = this.addRow.bind(this);
+        this.showCard = this.showCard.bind(this);
+        this.getEditData = this.getEditData.bind(this);
     }
 
     getDeleteData(deleted_item_name, deleted_item_id) {
@@ -25,8 +30,13 @@ class Shop extends React.Component {
                         showRequest: true});
     }
 
-    getSelectData(item_id) {
-        this.setState({selected: item_id});
+    getSelectData(item) {
+        this.setState({selected_item: item, showCard: true,});
+    }
+
+    getEditData(boolean) {
+        console.log(boolean);
+        this.setState({edit: boolean});
     }
 
     componentDidMount() {
@@ -39,9 +49,9 @@ class Shop extends React.Component {
         var result = goods.map((item) => {
             var props = {
                 item: item,
-                selected: this.state.selected,
+                selected: this.state.selected_item.id,
             };
-            return <Item key = {item.id} {...props} onItemDelete = {this.getDeleteData} onItemSelect = {this.getSelectData}/>});        
+            return <Item key = {item.id} {...props} onItemDelete = {this.getDeleteData} onItemSelect = {this.getSelectData} onItemEdit = {this.getEditData}/>});        
         return result;        
     }
 
@@ -54,8 +64,39 @@ class Shop extends React.Component {
         var pos = new_goods.findIndex((e) => (e.id === this.state.deleted_item_id));
         new_goods.splice(pos, 1);
         this.setState({goods: new_goods,
-                        showRequest: false});
+                        showRequest: false,
+                        showCard: false,
+                        selected_item: {},});
                         
+    }
+
+    addRow() {
+        console.log("row added");
+    }
+
+    showCard() {
+        var card;
+        if (this.state.edit) {
+            card = <div className = {(this.state.showCard) ? "card" : "invisible"}>
+                        <h3>Item Info</h3>
+                        <label htmlFor="name_inp">Name:</label>
+                        <input id = "name_inp" type="text"></input>
+
+                        <label htmlFor="price_inp">Price:</label>
+                        <input id = "price_inp" type="text"></input>
+
+                        <label htmlFor="stock_inp">In stock:</label>
+                        <input id = "stock_inp" type="text"></input>
+                    </div>;
+        } else {
+            card = <div className = {(this.state.showCard) ? "card" : "invisible"}>
+                        <h3>Item Info</h3>
+                        <p>Name: {this.state.selected_item.name}</p>
+                        <p>Price: {this.state.selected_item.price}</p>
+                        <p>In stock: {this.state.selected_item.stock}</p>
+                    </div>;
+        }
+        return card;
     }
 
     render() { 
@@ -63,20 +104,24 @@ class Shop extends React.Component {
         return (
             <div>
                 <h3>{this.props.shop_name}</h3>
-                <table>
-                    <thead>
-                        <tr key = "header">
-                            <th key = "name">Name</th>
-                            <th key = "price">Price</th>
-                            <th key = "pic">Picture</th>
-                            <th key = "stock">In stock</th>
-                            <th key = "contr">Control</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.createRows()}
-                    </tbody>
-                </table>
+                <div className = "wrapper">
+                    <table>
+                        <thead>
+                            <tr key = "header">
+                                <th key = "name">Name</th>
+                                <th key = "price">Price</th>
+                                <th key = "pic">Picture</th>
+                                <th key = "stock">In stock</th>
+                                <th key = "contr">Control</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.createRows()}
+                        </tbody>
+                    </table>
+                    {this.showCard()}
+                </div>
+                <input type="button" value="New" key = "new" onClick = {this.addRow}></input>
                 <div className = {(this.state.showRequest) ? "" : "invisible"}>
                     <h3>Do you realy want to delete {this.state.deleted_item_name}</h3>
                     <input type="button" value="Yes" key = "yes" onClick = {this.deleteRow}></input>
