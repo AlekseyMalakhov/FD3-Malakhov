67b8a5b39f1082f125cf6d1fb71ad5d5cb0d062e
Aleksey Malakhov <hexel@tut.by>
2020-03-22 11:19:12 +0300

    Update index.js

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -4,4 +4,29 @@ import './index.css';
 import Mobile from './Mobile.js';
 import Initial_data from './Initial_data.json';
 
+
+function addEditProperty() {
+    console.log(Initial_data);
+    /*
+    var init_data = {...Initial_data};
+    var id;
+    for (id in init_data.clients) {
+        console.log(id);
+        init_data.clients[id].forEach((e) => {e.edit = "false"})
+    }  
+    */
+    var id;
+    for (id in Initial_data.clients) {
+        var clients = Initial_data.clients[id];
+        clients.forEach((e) => {
+            var client = {...e};
+            console.log(client);
+            client.edit = "false";
+            });
+    }
+    
+}
+
+addEditProperty();
+
 ReactDOM.render(<Mobile initial_data = {Initial_data}></Mobile>, document.getElementById('root'));
